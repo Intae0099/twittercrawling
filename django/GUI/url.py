b263e0358df4b13bf2654cc6845ddf6fd8c48ab8
Intae0099 <81225723+Intae0099@users.noreply.github.com>
2022-01-18 12:22:42 +0900

    Update url.py

--- django/GUI/url.py
+++ django/GUI/url.py
@@ -18,10 +18,18 @@ from django.urls import path, include
 from crawling import views
 
 urlpatterns = [
-    path('', views.example, name='example'),
+    #API 키 확인 화면
+    path('', views.index, name='index'),
+    #키 확인 일치하면 넘어가는 화면
+    path('check/', views.checkAPI, name='check'),
+    #키 확인 없이 볼 수 있는화면
+    path('index/', views.example, name='example'),
+    #검색 후 넘어가는 화면
     path('search/', views.search, name='search'),
+    #새로고침 화면
     path('', views.refresh, name='refresh'),
-    #path('ws/stream/', views.stream, name='stream'),
+    #주문 취소 후 화면
     path('cancel/', views.cancelorder, name='cancel'),
+    #구매 후 넘어가는 화면
     path('purchase/', views.purchase, name='purchase'),
 ]
